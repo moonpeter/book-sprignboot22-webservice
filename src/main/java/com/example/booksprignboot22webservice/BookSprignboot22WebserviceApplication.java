427ff00de5f542f043b77eaf12e693d28af9e310
moonpeter <moonpeter@kakao.com>
2020-07-19 17:07:35 +0900

    Initial commit

---
+++ src/main/java/com/example/booksprignboot22webservice/BookSprignboot22WebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.booksprignboot22webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSprignboot22WebserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookSprignboot22WebserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/booksprignboot22webservice/BookSprignboot22WebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.booksprignboot22webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSprignboot22WebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
