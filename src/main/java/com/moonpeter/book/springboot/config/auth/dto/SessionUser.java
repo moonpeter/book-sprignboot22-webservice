1dafaa31e8f9b252ff5c5facb195265ce8e2cffb
kyoungho.moon <moonpeter@kakao.com>
2020-07-21 18:22:36 +0900

    5.3 구글 로그인 연동하기

---
+++ src/main/java/com/moonpeter/book/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,19 @@
+package com.moonpeter.book.springboot.config.auth.dto;
+
+import com.moonpeter.book.springboot.domain.user.User;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
