f622b81f661eea4f373c7c681c764304ef46505d
kyoungho.moon <moonpeter@kakao.com>
2020-07-21 11:28:00 +0900

    4.3 게시글 등록 화면 만들기

--- src/main/java/com/moonpeter/book/springboot/web/IndexController.java
+++ src/main/java/com/moonpeter/book/springboot/web/IndexController.java
@@ -1,15 +1,20 @@
 package com.moonpeter.book.springboot.web;
 
+import com.moonpeter.book.springboot.service.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
